bfced324d4a3a2e5afd763076d3f9863c3ab80e2
abel533 <abel533@gmail.com>
2013-09-23 22:14:18 +0800

    后缀模式插件完成。

---
+++ src/com/isea/plugin/fairyring/FairyringDownLoad.java
@@ -0,0 +1,65 @@
+package com.isea.plugin.fairyring;
+
+import java.util.List;
+
+import com.isea.basic.BaseDownload;
+import com.isea.basic.StopException;
+
+public class FairyringDownLoad extends BaseDownload{
+	/**
+	 * 批量下载图片
+	 * @param savePath
+	 * @param url
+	 * @param selector
+	 * @param page
+	 * @param pageSize
+	 * @param first true - 首页不同
+	 */
+	public void downloadMore(final String savePath, 
+			final String url, 
+			final String selector,
+			final String page,
+			final int start,
+			final int pageSize,
+			final boolean first){
+		isstart = true;
+		String childUrl = null;
+		String tempUrl = url;
+		if(!tempUrl.endsWith("/")){
+			tempUrl += "/";
+		}
+		int _start = start;
+		int max = start + pageSize;
+		int count = 1;
+		List<String> list = null;
+		
+		for(;_start<max;_start++,count++){
+			if(isstart){
+				tips("正在下载 "+count+"/"+pageSize);
+				if(_start==1&&first){
+					childUrl = url;
+				}
+				else{
+					childUrl = tempUrl + _start + page;
+				}
+				try {
+					log("开始下载地址:"+childUrl);
+					list = getSrcPath(childUrl, selector);
+					downLoadImages(list, savePath);
+				} catch (StopException e) {
+					//停止下载
+					log(e.getMessage());
+					break;
+				} catch (Exception e) {
+					log(e.getMessage());
+				}
+			} else {
+				log("终止下载!");
+			}
+		}
+		if(isstart){
+			tips("下载完成");
+			progress(100);
+		}
+	}
+}
---
+++ src/com/isea/plugin/fairyring/FairyringPanel.java
@@ -0,0 +1,162 @@
+package com.isea.plugin.fairyring;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import com.isea.basic.BasePanel;
+
+
+public class FairyringPanel extends BasePanel {
+	private static final long serialVersionUID = 9080426160199827735L;
+	private JTextField url;
+	private JTextField page;
+	private JTextField selector;
+	private JTextField pageSize;
+	private JRadioButton firstRadio;
+	private JTextField start;
+	
+	public FairyringPanel() {
+		setBorder(new EmptyBorder(2, 4, 2, 0));
+		setLayout(null);
+		
+		JLabel lblhttpwwwblogcompage = new JLabel("该模式针对地址为：http://www.********.com/***/5.html 类似地址的网站");
+		lblhttpwwwblogcompage.setBounds(69, 10, 431, 26);
+		add(lblhttpwwwblogcompage);
+		lblhttpwwwblogcompage.setHorizontalAlignment(SwingConstants.CENTER);
+		lblhttpwwwblogcompage.setPreferredSize(new Dimension(200, 26));
+		
+		firstRadio = new JRadioButton("首地址不带页码");
+		firstRadio.setSelected(true);
+		firstRadio.setBounds(70, 130, 116, 23);
+		add(firstRadio);
+		
+		JLabel label_1 = new JLabel("首地址：");
+		label_1.setBounds(0, 135, 52, 15);
+		add(label_1);
+		
+		JLabel lblNewLabel = new JLabel("基础地址：");
+		lblNewLabel.setBounds(0, 54, 65, 15);
+		add(lblNewLabel);
+		
+		url = new JTextField();
+		url.setText("http://fairyring.beon.ru");
+		url.setBounds(70, 46, 420, 30);
+		add(url);
+		url.setColumns(10);
+		
+		JLabel label = new JLabel("后缀：");
+		label.setBounds(0, 95, 65, 15);
+		add(label);
+		
+		page = new JTextField();
+		page.setBounds(70, 87, 420, 30);
+		add(page);
+		page.setText(".html");
+		page.setColumns(10);
+		
+		JLabel label_2 = new JLabel("图片选择器：");
+		label_2.setBounds(0, 170, 86, 15);
+		add(label_2);
+		
+		selector = new JTextField();
+		selector.setText("div.c img");
+		selector.setBounds(70, 163, 420, 30);
+		add(selector);
+		selector.setColumns(10);
+		
+		JLabel label_3 = new JLabel("页码限制：");
+		label_3.setBounds(360, 135, 69, 15);
+		add(label_3);
+		
+		pageSize = new JTextField();
+		pageSize.setHorizontalAlignment(SwingConstants.RIGHT);
+		pageSize.setText("1");
+		pageSize.setBounds(435, 127, 55, 30);
+		add(pageSize);
+		pageSize.setColumns(10);
+		
+		JLabel label_4 = new JLabel("起始页码：");
+		label_4.setBounds(200, 135, 69, 15);
+		add(label_4);
+		
+		start = new JTextField();
+		start.setHorizontalAlignment(SwingConstants.RIGHT);
+		start.setText("1");
+		start.setBounds(280, 126, 55, 30);
+		add(start);
+		start.setColumns(10);
+
+	}
+
+	/**
+	 * 下载按钮执行的方法
+	 */
+	@Override
+	public void download(String savePath) {
+		if(!checkField()){
+			return;
+		}
+		progress(0);
+		log("开始下载");
+		log("保存路径:"+savePath);
+		this.setDownloadHelper(new FairyringDownLoad());
+		downloadHelper.downloadMore(savePath, 
+				url.getText(), 
+				selector.getText(), 
+				page.getText(),
+				Integer.parseInt(start.getText()),
+				Integer.parseInt(pageSize.getText()), 
+				firstRadio.isSelected());
+	}
+
+	/**
+	 * 校验用户填入信息
+	 */
+	private boolean checkField(){
+		if(url.getText().equals("")){
+			JOptionPane.showMessageDialog(FairyringPanel.this, "基础地址不能为空", "提示", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		if(selector.getText().equals("")){
+			JOptionPane.showMessageDialog(FairyringPanel.this, "图片选择器不能为空", "提示", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		if(page.getText().equals("")){
+			JOptionPane.showMessageDialog(FairyringPanel.this, "后缀不能为空", "提示", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		if(start.getText().equals("")){
+			JOptionPane.showMessageDialog(FairyringPanel.this, "起始页码不能为空", "提示", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		if(pageSize.getText().equals("")){
+			JOptionPane.showMessageDialog(FairyringPanel.this, "页码限制不能为空", "提示", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public void clear() {
+		//清空界面信息
+		url.setText("");
+		selector.setText("");
+		page.setText("");
+		start.setText("");
+		pageSize.setText("");
+		firstRadio.setSelected(true);
+	}
+
+	@Override
+	public void stop() {
+		//停止下载
+		if(downloadHelper!=null){
+			downloadHelper.stop();
+		}
+	}
+}
